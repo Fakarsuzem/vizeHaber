a89a84ed2eaa130a5f88cbc62cb95191293e8091
Fakarsuzem <gizemfakarsu@outlook.com>
2022-04-30 14:19:30 +0300

    şema resmi

--- vize/vize/haber/Models/ModelDB.Designer.cs
+++ vize/vize/haber/Models/ModelDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Pc\Desktop\vize\vize\haber\Models\ModelDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Pc\Desktop\vizeGithub\vizeHaber\vize\vize\haber\Models\ModelDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
